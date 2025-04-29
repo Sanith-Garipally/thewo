14101d9ba2d6936874cef8a8c6299826975f4dfc
Sanith Garipally <sanith.garipally.19.1@mountblue.tech>
2025-04-29 21:14:58 +0530

    Add: Auth Logic to Routes

--- app/_components/Reservations.js
+++ app/_components/Reservations.js
@@ -1,14 +1,24 @@
 import DateSelector from '@/app/_components/DateSelector';
 import ReservationForm from '@/app/_components/ReservationForm';
-import { getBookedDatesByCabinId, getSettings } from "@/app/_lib/data-service";
-
+import { getBookedDatesByCabinId, getSettings } from '@/app/_lib/data-service';
+import { auth } from '@/app/_lib/auth';
+import LoginMessage from '@/app/_components/LoginMessage';
 
 async function Reservations({ cabin }) {
-    const [settings, bookedDates] = await Promise.all([getSettings(), getBookedDatesByCabinId(cabin.id)]);
+  const session = await auth();
+  const [settings, bookedDates] = await Promise.all([
+    getSettings(),
+    getBookedDatesByCabinId(cabin.id),
+  ]);
   return (
     <div className='grid grid-cols-2 border border-primary-800 min-h-[500px]'>
-      <DateSelector className='mb-4' settings={settings} bookedDates={bookedDates} cabin={cabin}/>
-      <ReservationForm cabin={cabin}/>
+      <DateSelector
+        className='mb-4'
+        settings={settings}
+        bookedDates={bookedDates}
+        cabin={cabin}
+      />
+      {session?.user ? <ReservationForm cabin={cabin} user={session.user}/> : <LoginMessage />}
     </div>
   );
 }
---
+++ app/account/page.js
@@ -0,0 +1,16 @@
+import { auth } from '@/app/_lib/auth';
+
+export const metadata = {
+  title: "Guest Area",
+};
+
+const Account= async() => {
+  const session = await auth();
+  return (
+    <h2 className="font-semibold text-2xl text-accent-400 mb-7">
+    Welcome {session?.user?.name}!
+  </h2>
+  )
+}
+
+export default Account
